3a4418d0f2555f56693b633e2198a484040ec9a5
Vladimir <VladimirNovikovski@gmail.com>
2022-10-16 23:28:58 +0300

    new decision if

--- Program.cs
+++ Program.cs
@@ -2,5 +2,10 @@
 string[] newarray = new string[6];
 int i = 0;
 for (i = 0; i < array.Length; i++){
-    
+    if (array[i].Length <= 3)
+    {
+        newarray[i] = array[i];
+        Console.Write($"{newarray[i]}");
+    }
+
 }
